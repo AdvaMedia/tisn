1261d21d157766f220d3a7d9a487a1d8d2b74f97
Alex Kirillov <saratovsource@gmail.com>
2011-04-27 14:52:23 +0400

    Добавил конфиг единорога и немного изменил деплой

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,9 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) 
+require "rvm/capistrano"
+
+set :using_rvm, true
+set :rvm_ruby_string, 'rvm 1.8.7@tins'
+
 set :application, "tisn.preview"
 
 role :web, "advamedia.ru"                          # Your HTTP server, Apache/etc
@@ -14,3 +20,21 @@ set :scm, :git
 set :repository, "git@rubylnik.ru:tisn.git"
 set :branch, "faq"
 set :deploy_via, :remote_cache
+
+after "deploy:setup" do
+  run "mkdir -p #{deploy_to}/shared/pids && mkdir -p #{deploy_to}/shared/config && mkdir -p #{deploy_to}/shared/var"
+end
+
+namespace :unicorn do
+  task :start do
+    run "cd #{deploy_to}/current && unicorn_rails -c #{deploy_to}/current/config/unicorn.rb -e #{rails_env} -D"
+  end
+ 
+  task :stop do
+    run "kill -9 `cat #{deploy_to}/shared/pids/unicorn.pid`"
+  end
+ 
+  task :restart do
+    run "kill -HUP `cat #{deploy_to}/shared/pids/unicorn.pid`"
+  end
+end
